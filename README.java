70dac968a6a2eb37900e1809179c3ead2b638b33
abi123naya <connect2abinaya@gmail.com>
2016-07-08 11:40:31 +0530

    Update and rename README.md to README.java
    finish

---
+++ README.java
@@ -0,0 +1,23 @@
+# sandal-wood
+import.java.util.*;
+class Summer
+{ public static void main(String[] args)
+{ int x,sum=0;
+ int a[]=new a[10];
+  System.out.println("Enter limit");
+  Scanner s1=new Scanner(System.in);
+  x=s1.nextInt();
+  for(int i=1;i<=x;i++)
+   {System.out.println("Enter numbers");
+   Scanner s2=new Scanner(System.in);
+  a[i]=s1.nextInt();
+   }
+   void add()
+    {  for(int i=1;i<=x;i++)
+    sum=sum+a[i]
+    
+    }
+ }
+
+
+}
